ba1eb45971e7a406bc7094fd14bf2f0db77448ac
ansha-1998 <mohammedansha14@gmail.com>
2021-05-18 20:29:54 +0530

    hello world

--- largest.py
+++ largest.py
@@ -1,16 +1,1 @@
-def largest():
-	y = 0
-	for i in range (3):
-		x = input()
-		if y<=x:
-			y = x
-	print y
-largest()				
-def smallest():
-	y = 25000000
-	for i in range (3):
-		x = input()
-		if y>=x:
-			y = x
-	print y
-smallest()	
+print "Hello world"
